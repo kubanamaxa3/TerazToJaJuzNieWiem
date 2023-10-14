813a7a16bb75b546843836dd32d959a17294d995
kubanamaxa2 <jakubniemczyk09@gmail.com>
2023-10-15 00:50:51 +0200

    Dodano liste view

--- app/src/main/java/com/example/teraztojajuzniewiem/MainActivity.kt
+++ app/src/main/java/com/example/teraztojajuzniewiem/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.teraztojajuzniewiem
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import android.widget.ImageButton
 import android.widget.SeekBar
 import android.widget.TextView
@@ -11,8 +12,12 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         var lista = arrayOf(findViewById<ImageButton>(R.id.btn1),findViewById<ImageButton>(R.id.btn2),findViewById<ImageButton>(R.id.btn3),findViewById<ImageButton>(R.id.btn4),findViewById<ImageButton>(R.id.btn5))
+        var listaView = arrayOf<TextView>(findViewById(R.id.view12),findViewById(R.id.view13),findViewById(R.id.view14),findViewById(R.id.view15),findViewById(R.id.view21),
+            findViewById(R.id.view23),findViewById(R.id.view24),findViewById(R.id.view25),findViewById(R.id.view31),findViewById(R.id.view32),findViewById(R.id.view34),findViewById(R.id.view35),
+            findViewById(R.id.view41),findViewById(R.id.view42),findViewById(R.id.view43),findViewById(R.id.view45),findViewById(R.id.view51),findViewById(R.id.view52),findViewById(R.id.view53),
+            findViewById(R.id.view54),)
         var lista2 = arrayListOf<ImageButton>()
-        var lista3 = arrayListOf<Int>()
+        var wzorzec = "2,3"
         var wartosc = 0
 
 
@@ -44,10 +49,13 @@ class MainActivity : AppCompatActivity() {
                     }
                 }
 
-                findViewById<TextView>(R.id.textView6).text = lista3.toString()
-                findViewById<TextView>(R.id.textView3).text = lista2.size.toString()
+               /* findViewById<TextView>(R.id.textView6).text = lista3.toString() */
+               /* findViewById<TextView>(R.id.textView3).text = lista2.size.toString()*/
             }
 
+        }
+        findViewById<Button>(R.id.button).setOnClickListener {
+            findViewById<TextView>(R.id.textView6).text = listaView.size.toString()
         }
         findViewById<SeekBar>(R.id.seekBar)?.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
             override fun onProgressChanged(
